a9f69f2f49f9ab8d36a6c175e814628c15a87b4c
Annorino <132631480+Annorinoo@users.noreply.github.com>
2023-09-21 18:38:18 -0700

    "commiting db and api edits"

---
+++ src/server/db/reviews.js
@@ -0,0 +1,80 @@
+const client = require('./client');
+
+//database functions
+async function getAllReviews() {
+    try {
+        const {rows} = await client.query(`
+        SELECT * FROM reviews;
+        `);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getReviewById(id) {
+    try {
+        const {rows: [review]} = await client.query(`
+        SELECT * FROM reviews
+        WHERE id = $1
+        `, [id]);
+        return review;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getReviewByName(name) {
+    try {
+        const {rows: [review]} = await client.query(`
+        SELECT * FROM reviews
+        WHERE name = $1
+        `, [name]);
+        return review;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function createReview({ websiteId, userId, name, content, rating, date }) {
+    try {
+        const {rows: [review]} = await client.query(`
+        INSERT INTO reviews(websiteId, userId, name, content, rating, date) VALUES ($1, $2, $3, $4, $5, $6)
+        ON CONFLICT (name) DO NOTHING
+        RETURNING *
+        `, [websiteId, userId, name, content, rating, date]);
+        return review;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function updateReview({id, ...fields}){
+    try {
+      const toUpdate = {}
+      for(let column in fields) {
+        if(fields[column] !== undefined) toUpdate[column] = fields[column];
+      }
+      let review;
+      if (util.dbFields(toUpdate).insert.length > 0) {
+        const {rows} = await client.query(`
+          UPDATE reviews
+          SET ${ util.dbFields(toUpdate).insert }
+          WHERE id=${ id }
+          RETURNING *;
+        `, Object.values(toUpdate));
+        review = rows[0];
+      }
+      return review;
+    } catch (error) {
+      throw error
+    }
+}
+
+module.exports = {
+    getAllReviews,
+    getReviewById,
+    getReviewByName,
+    createReview,
+    updateReview,
+}
